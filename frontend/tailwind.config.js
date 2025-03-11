d1af331885de4008be5edf321b5b590e32ecb5d0
Ayush <payush548@gmail.com>
2025-03-10 23:44:03 -0400

    tailwind configured

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,17 @@
-export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#0D0D0D", // Black
-          accent1: "#00A878", // Green 1 (Bold)
-          accent2: "#75D99F", // Green 2 (Light)
-          secondary: "#1A1A1A", // Dark Grey
-        },
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: "#0D0D0D", // Black
+        accent1: "#00A878", // Green 1 (Bold)
+        accent2: "#75D99F", // Green 2 (Light)
+        secondary: "#1A1A1A", // Dark Grey
       },
     },
-    plugins: [],
-  };
-  
-  
+  },
+  plugins: [],
+};
