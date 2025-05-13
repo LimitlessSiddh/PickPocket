9cbb87f108b06d2cb5c86a8e822995583322f296
Ayush <payush548@gmail.com>
2025-05-12 21:52:06 -0400

    adding routes and setting up model

--- backend/python_backend/ai_model/model.py
+++ backend/python_backend/ai_model/model.py
@@ -5,3 +5,92 @@ from xgboost import XGBRegressor, XGBClassifier
 import pandas as pd
 import numpy as np
 from fetch_data import get_historical_data
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+
+class AIModel():
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        self.model_type = model_type
+        self.data = data
+
+    def train_model(self):
+        pass
+    
+    def predict(home: str, away: str):
+        pass
+    
+    def evaluate(self):
+        pass
+
+
+class NBA_AIModel(AIModel):
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        super().__init__(model_type, data)
+
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
+
+class NFL_AIModel(AIModel):
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        super().__init__(model_type, data)
+
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
+
+class UFC_AIModel(AIModel):
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        super().__init__(model_type, data)
+
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
+
+
+class MLB_AIModel(AIModel):
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        super().__init__(model_type, data)
+
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
+
+
+class NHL_AIModel(AIModel):
+    def __init__(self, model_type: str, data: pd.DataFrame):
+        super().__init__(model_type, data)
+
+    def train_model(self):
+        pass
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
--- backend/python_backend/python_routes/routes.py
+++ backend/python_backend/python_routes/routes.py
@@ -1,1 +1,52 @@
-import fastapi
+from fastapi import FastAPI
+from pydantic import BaseModel
+from ai_model.model import NBA_AIModel, NFL_AIModel, UFC_AIModel, MLB_AIModel, NHL_AIModel
+
+app = FastAPI()
+# use the /docs# route to get a gui for testing api
+
+class MatchResult(BaseModel):
+    """
+    This represents the predicted result of a match.
+    Home or Away
+    """
+    result: str
+
+class FightResult(BaseModel):
+    """
+    This represents the predicted result of a fight.
+    Fighter1 or Fighter2
+    """
+    result: str = "Tied"
+
+
+
+@app.get("/get_nba_bet_data/{home}/{away}", response_model=MatchResult)
+async def get_nba_bet_data(home: str, away: str) -> MatchResult:
+    nba_model = NBA_AIModel()
+    result = nba_model.predict(home, away)
+    return {"result": result}
+
+@app.get("/get_nfl_bet_data/{home}/{away}", response_model=MatchResult)
+async def get_nfl_bet_data(home: str, away: str) -> MatchResult:
+    nfl_model = NFL_AIModel()
+    result = nfl_model.predict(home, away)
+    return {"result": result}
+
+@app.get("/get_ufc_bet_data/{fighter1}/{fighter2}", response_model=FightResult)
+async def get_ufc_bet_data(fighter1: str, fighter2: str) -> FightResult:
+    ufc_model = UFC_AIModel()
+    result = ufc_model.predict(fighter1, fighter2)
+    return {"result": result}
+
+@app.get("/get_mlb_bet_data/{home}/{away}", response_model=MatchResult)
+async def get_mlb_bet_data(home: str, away: str) -> MatchResult:
+    mlb_model = MLB_AIModel()
+    result = mlb_model.predict(home, away)
+    return {"result": result}
+
+@app.get("/get_nhl_bet_data/{home}/{away}", response_model=MatchResult)
+async def get_nhl_bet_data(home: str, away: str) -> MatchResult:
+    nhl_model = NHL_AIModel()
+    result = nhl_model.predict(home, away)
+    return {"result": result}
